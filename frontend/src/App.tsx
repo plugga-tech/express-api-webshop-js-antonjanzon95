8c74619b570af91cada960f1dc74a7b8649dffda
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-03-21 21:20:48 +0100

    added tailwind, added test fetch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+interface User {
+  _id: string;
+  name: string;
+  email: string;
+}
 
 function App() {
+  const [test, setTest] = useState<User[]>([]);
+
+  useEffect(() => {
+    let unsubscribed = false;
+    const fetchUsers = async () => {
+      const response = await fetch("http://localhost:3000/api/users");
+      const data = await response.json();
+      if (!unsubscribed) {
+        console.log(data);
+        setTest(data);
+      }
+    };
+    fetchUsers();
+
+    return () => {
+      unsubscribed = true;
+    };
+  }, []);
+
   return (
     <div className="flex justify-center items-center w-screen h-screen">
       Hello World
+      <div>
+        <ul>
+          {test.map((user) => (
+            <li key={user._id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
